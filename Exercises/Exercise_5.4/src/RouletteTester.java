285f6defdb5442762d90c567d647d6e8196fb8f9
Severin Kaderli <severin.kaderli@gmail.com>
2017-10-27 10:57:34 +0200

    Change number of iterations
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- Exercises/Exercise_5.4/src/RouletteTester.java
+++ Exercises/Exercise_5.4/src/RouletteTester.java
@@ -2,7 +2,7 @@ public class RouletteTester {
     public static void main(String[] args) {
         RoulettePlayer simulator = new RoulettePlayer();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100000; i++) {
             simulator.visitCasino(100, 200);
         }
 
